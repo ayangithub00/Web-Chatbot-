520fd855bccc7b1582a2cec6e7dc853e67b62b33
Ayan Islam <ayanislam78600@gmail.com>
2026-06-10 12:37:54 +0530

    add Dockerfile

--- Chatbot/app/views.py
+++ Chatbot/app/views.py
@@ -46,8 +46,6 @@ class ChatView(APIView):
 
         Embedding = HuggingFaceEndpointEmbeddings(
         model_name="BAAI/bge-small-en-v1.5",
-        model_kwargs={"device": "cpu"},
-        encode_kwargs={"normalize_embeddings": True}
         )
         
         # Storing In Vector 
